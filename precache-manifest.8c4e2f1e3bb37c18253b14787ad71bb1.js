256ecb8621662230c661f1b68e55065c740f2ff5
“IDerevyansky” <IDerevyansky@yandex.ru>
2019-05-13 19:20:39 +0300

    -

---
+++ precache-manifest.8c4e2f1e3bb37c18253b14787ad71bb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "0ffb34b2ff035cdd15cc",
+    "url": "/static/js/main.d6eadd3b.chunk.js"
+  },
+  {
+    "revision": "91b71caedb12ebdadee4",
+    "url": "/static/js/2.155fb25f.chunk.js"
+  },
+  {
+    "revision": "0ffb34b2ff035cdd15cc",
+    "url": "/static/css/main.dc68ea01.chunk.css"
+  },
+  {
+    "revision": "91b71caedb12ebdadee4",
+    "url": "/static/css/2.0072cc78.chunk.css"
+  },
+  {
+    "revision": "b23145f16679d65f35bce2c15b6fdc30",
+    "url": "/index.html"
+  }
+];
