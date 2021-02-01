0ca098b1960836f4c189d29cfaea40294ddeabc5
“IDerevyansky” <IDerevyansky@yandex.ru>
2021-02-01 12:02:44 +0300

    -

--- precache-manifest.187ecd5bbcf7e2f07d7d522f7e40c72b.js
+++ precache-manifest.187ecd5bbcf7e2f07d7d522f7e40c72b.js
@@ -1,103 +1,103 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
     "revision": "ae6120f7cdf769ab846b8fcdc695e999",
-    "url": "/index.html"
+    "url": "index.html"
   },
   {
     "revision": "c26a3b4a6f0d19701c45",
-    "url": "/static/css/main.3baf54c8.chunk.css"
+    "url": "static/css/main.3baf54c8.chunk.css"
   },
   {
     "revision": "346d8691edde8b1789c6",
-    "url": "/static/js/2.1cb92cf8.chunk.js"
+    "url": "static/js/2.1cb92cf8.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/static/js/2.1cb92cf8.chunk.js.LICENSE.txt"
+    "url": "static/js/2.1cb92cf8.chunk.js.LICENSE.txt"
   },
   {
     "revision": "c26a3b4a6f0d19701c45",
-    "url": "/static/js/main.337fbda0.chunk.js"
+    "url": "static/js/main.337fbda0.chunk.js"
   },
   {
     "revision": "c1621f2b343c0bac21e9",
-    "url": "/static/js/runtime-main.96678d91.js"
+    "url": "static/js/runtime-main.96678d91.js"
   },
   {
     "revision": "104be5f79e3ef6239d62bd897fde8d91",
-    "url": "/static/media/AvenirNextCyr-Demi.104be5f7.ttf"
+    "url": "static/media/AvenirNextCyr-Demi.104be5f7.ttf"
   },
   {
     "revision": "5279d5a5c82ae79515ecaf462d0357c1",
-    "url": "/static/media/AvenirNextCyr-DemiItalic.5279d5a5.ttf"
+    "url": "static/media/AvenirNextCyr-DemiItalic.5279d5a5.ttf"
   },
   {
     "revision": "c91f454865c62e8e068ce224a49fccc9",
-    "url": "/static/media/AvenirNextCyr-Light.c91f4548.ttf"
+    "url": "static/media/AvenirNextCyr-Light.c91f4548.ttf"
   },
   {
     "revision": "862c7c4267856b43beabe738a13c281e",
-    "url": "/static/media/AvenirNextCyr-Medium.862c7c42.ttf"
+    "url": "static/media/AvenirNextCyr-Medium.862c7c42.ttf"
   },
   {
     "revision": "f2fc32fb724bca617be17961d4a846d2",
-    "url": "/static/media/AvenirNextCyr-MediumItalic.f2fc32fb.ttf"
+    "url": "static/media/AvenirNextCyr-MediumItalic.f2fc32fb.ttf"
   },
   {
     "revision": "97b615b907fd3510f9129eac4a731f6f",
-    "url": "/static/media/AvenirNextCyr-Regular.97b615b9.ttf"
+    "url": "static/media/AvenirNextCyr-Regular.97b615b9.ttf"
   },
   {
     "revision": "7a91f42b6c29e0c54ae8b9b1ccf715f4",
-    "url": "/static/media/DEREVYANSKAYA-gray.7a91f42b.svg"
+    "url": "static/media/DEREVYANSKAYA-gray.7a91f42b.svg"
   },
   {
     "revision": "68375d4a128e3d47d4ca6804995b182b",
-    "url": "/static/media/DEREVYANSKAYA.68375d4a.svg"
+    "url": "static/media/DEREVYANSKAYA.68375d4a.svg"
   },
   {
     "revision": "6980bca31f5a4e9782825fa17a206e37",
-    "url": "/static/media/FB.6980bca3.svg"
+    "url": "static/media/FB.6980bca3.svg"
   },
   {
     "revision": "63d5a3af28b49a9fa02b85f4d701e96a",
-    "url": "/static/media/btnSingUp-gray.63d5a3af.svg"
+    "url": "static/media/btnSingUp-gray.63d5a3af.svg"
   },
   {
     "revision": "bb7efa1a3d652ccdd85f08caa3369890",
-    "url": "/static/media/btnSingUp.bb7efa1a.svg"
+    "url": "static/media/btnSingUp.bb7efa1a.svg"
   },
   {
     "revision": "64e38aa6866ced15f8c3e37f8ea8548e",
-    "url": "/static/media/closeIcon.64e38aa6.svg"
+    "url": "static/media/closeIcon.64e38aa6.svg"
   },
   {
     "revision": "3cb1a6231514ceb10ba854e242621b47",
-    "url": "/static/media/crossClosed.3cb1a623.svg"
+    "url": "static/media/crossClosed.3cb1a623.svg"
   },
   {
     "revision": "075e528093644a609f51a1af1c762b26",
-    "url": "/static/media/face_main_banner.075e5280.png"
+    "url": "static/media/face_main_banner.075e5280.png"
   },
   {
     "revision": "ebddb667b6d3dd473f78a7d592b520b9",
-    "url": "/static/media/inst.ebddb667.svg"
+    "url": "static/media/inst.ebddb667.svg"
   },
   {
     "revision": "6f11557e0dc1e78f0b65227ec1a2fe8a",
-    "url": "/static/media/mainPage.6f11557e.png"
+    "url": "static/media/mainPage.6f11557e.png"
   },
   {
     "revision": "c139b0f1ebb2ef2ff4cb06c47c20c4bb",
-    "url": "/static/media/mobileMenu.c139b0f1.svg"
+    "url": "static/media/mobileMenu.c139b0f1.svg"
   },
   {
     "revision": "f4577e663a6ffcd665939ccc1a317e63",
-    "url": "/static/media/telegram-gray.f4577e66.svg"
+    "url": "static/media/telegram-gray.f4577e66.svg"
   },
   {
     "revision": "4ca882dafcd5670fa1847a64b81df734",
-    "url": "/static/media/telegram.4ca882da.svg"
+    "url": "static/media/telegram.4ca882da.svg"
   },
   {
     "revision": "dd2a93702dc7a347dbc5da15b7b52bf6",
