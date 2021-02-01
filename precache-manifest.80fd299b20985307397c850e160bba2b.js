0bba583abf50393f94a53e4fee1d50a6be7a4448
“IDerevyansky” <IDerevyansky@yandex.ru>
2021-02-01 17:21:54 +0300

    -

--- precache-manifest.80fd299b20985307397c850e160bba2b.js
+++ precache-manifest.80fd299b20985307397c850e160bba2b.js
@@ -5,118 +5,118 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
   },
   {
     "revision": "f506aeda80825656e3a0",
-    "url": "/IDerevyansky/deploy/static/css/main.0ef8aed8.chunk.css"
+    "url": "/IDerevyansky/deploystatic/css/main.0ef8aed8.chunk.css"
   },
   {
     "revision": "346d8691edde8b1789c6",
-    "url": "/IDerevyansky/deploy/static/js/2.1cb92cf8.chunk.js"
+    "url": "/IDerevyansky/deploystatic/js/2.1cb92cf8.chunk.js"
   },
   {
     "revision": "c64c486544348f10a6d6c716950bc223",
-    "url": "/IDerevyansky/deploy/static/js/2.1cb92cf8.chunk.js.LICENSE.txt"
+    "url": "/IDerevyansky/deploystatic/js/2.1cb92cf8.chunk.js.LICENSE.txt"
   },
   {
     "revision": "f506aeda80825656e3a0",
-    "url": "/IDerevyansky/deploy/static/js/main.97d23301.chunk.js"
+    "url": "/IDerevyansky/deploystatic/js/main.97d23301.chunk.js"
   },
   {
     "revision": "c34ae6825be95cf8af41",
-    "url": "/IDerevyansky/deploy/static/js/runtime-main.710f1425.js"
+    "url": "/IDerevyansky/deploystatic/js/runtime-main.710f1425.js"
   },
   {
     "revision": "104be5f79e3ef6239d62bd897fde8d91",
-    "url": "/IDerevyansky/deploy/static/media/AvenirNextCyr-Demi.104be5f7.ttf"
+    "url": "/IDerevyansky/deploystatic/media/AvenirNextCyr-Demi.104be5f7.ttf"
   },
   {
     "revision": "5279d5a5c82ae79515ecaf462d0357c1",
-    "url": "/IDerevyansky/deploy/static/media/AvenirNextCyr-DemiItalic.5279d5a5.ttf"
+    "url": "/IDerevyansky/deploystatic/media/AvenirNextCyr-DemiItalic.5279d5a5.ttf"
   },
   {
     "revision": "c91f454865c62e8e068ce224a49fccc9",
-    "url": "/IDerevyansky/deploy/static/media/AvenirNextCyr-Light.c91f4548.ttf"
+    "url": "/IDerevyansky/deploystatic/media/AvenirNextCyr-Light.c91f4548.ttf"
   },
   {
     "revision": "862c7c4267856b43beabe738a13c281e",
-    "url": "/IDerevyansky/deploy/static/media/AvenirNextCyr-Medium.862c7c42.ttf"
+    "url": "/IDerevyansky/deploystatic/media/AvenirNextCyr-Medium.862c7c42.ttf"
   },
   {
     "revision": "f2fc32fb724bca617be17961d4a846d2",
-    "url": "/IDerevyansky/deploy/static/media/AvenirNextCyr-MediumItalic.f2fc32fb.ttf"
+    "url": "/IDerevyansky/deploystatic/media/AvenirNextCyr-MediumItalic.f2fc32fb.ttf"
   },
   {
     "revision": "97b615b907fd3510f9129eac4a731f6f",
-    "url": "/IDerevyansky/deploy/static/media/AvenirNextCyr-Regular.97b615b9.ttf"
+    "url": "/IDerevyansky/deploystatic/media/AvenirNextCyr-Regular.97b615b9.ttf"
   },
   {
     "revision": "7a91f42b6c29e0c54ae8b9b1ccf715f4",
-    "url": "/IDerevyansky/deploy/static/media/DEREVYANSKAYA-gray.7a91f42b.svg"
+    "url": "/IDerevyansky/deploystatic/media/DEREVYANSKAYA-gray.7a91f42b.svg"
   },
   {
     "revision": "68375d4a128e3d47d4ca6804995b182b",
-    "url": "/IDerevyansky/deploy/static/media/DEREVYANSKAYA.68375d4a.svg"
+    "url": "/IDerevyansky/deploystatic/media/DEREVYANSKAYA.68375d4a.svg"
   },
   {
     "revision": "6980bca31f5a4e9782825fa17a206e37",
-    "url": "/IDerevyansky/deploy/static/media/FB.6980bca3.svg"
+    "url": "/IDerevyansky/deploystatic/media/FB.6980bca3.svg"
   },
   {
     "revision": "63d5a3af28b49a9fa02b85f4d701e96a",
-    "url": "/IDerevyansky/deploy/static/media/btnSingUp-gray.63d5a3af.svg"
+    "url": "/IDerevyansky/deploystatic/media/btnSingUp-gray.63d5a3af.svg"
   },
   {
     "revision": "bb7efa1a3d652ccdd85f08caa3369890",
-    "url": "/IDerevyansky/deploy/static/media/btnSingUp.bb7efa1a.svg"
+    "url": "/IDerevyansky/deploystatic/media/btnSingUp.bb7efa1a.svg"
   },
   {
     "revision": "64e38aa6866ced15f8c3e37f8ea8548e",
-    "url": "/IDerevyansky/deploy/static/media/closeIcon.64e38aa6.svg"
+    "url": "/IDerevyansky/deploystatic/media/closeIcon.64e38aa6.svg"
   },
   {
     "revision": "3cb1a6231514ceb10ba854e242621b47",
-    "url": "/IDerevyansky/deploy/static/media/crossClosed.3cb1a623.svg"
+    "url": "/IDerevyansky/deploystatic/media/crossClosed.3cb1a623.svg"
   },
   {
     "revision": "075e528093644a609f51a1af1c762b26",
-    "url": "/IDerevyansky/deploy/static/media/face_main_banner.075e5280.png"
+    "url": "/IDerevyansky/deploystatic/media/face_main_banner.075e5280.png"
   },
   {
     "revision": "ebddb667b6d3dd473f78a7d592b520b9",
-    "url": "/IDerevyansky/deploy/static/media/inst.ebddb667.svg"
+    "url": "/IDerevyansky/deploystatic/media/inst.ebddb667.svg"
   },
   {
     "revision": "6f11557e0dc1e78f0b65227ec1a2fe8a",
-    "url": "/IDerevyansky/deploy/static/media/mainPage.6f11557e.png"
+    "url": "/IDerevyansky/deploystatic/media/mainPage.6f11557e.png"
   },
   {
     "revision": "c139b0f1ebb2ef2ff4cb06c47c20c4bb",
-    "url": "/IDerevyansky/deploy/static/media/mobileMenu.c139b0f1.svg"
+    "url": "/IDerevyansky/deploystatic/media/mobileMenu.c139b0f1.svg"
   },
   {
     "revision": "f4577e663a6ffcd665939ccc1a317e63",
-    "url": "/IDerevyansky/deploy/static/media/telegram-gray.f4577e66.svg"
+    "url": "/IDerevyansky/deploystatic/media/telegram-gray.f4577e66.svg"
   },
   {
     "revision": "4ca882dafcd5670fa1847a64b81df734",
-    "url": "/IDerevyansky/deploy/static/media/telegram.4ca882da.svg"
+    "url": "/IDerevyansky/deploystatic/media/telegram.4ca882da.svg"
   },
   {
     "revision": "dd2a93702dc7a347dbc5da15b7b52bf6",
-    "url": "/IDerevyansky/deploy/static/media/viber-gray.dd2a9370.svg"
+    "url": "/IDerevyansky/deploystatic/media/viber-gray.dd2a9370.svg"
   },
   {
     "revision": "fe9e7e798050d012dd2247691400068d",
-    "url": "/IDerevyansky/deploy/static/media/viber.fe9e7e79.svg"
+    "url": "/IDerevyansky/deploystatic/media/viber.fe9e7e79.svg"
   },
   {
     "revision": "73d44733607718af9e2b412f49f185b1",
-    "url": "/IDerevyansky/deploy/static/media/vk.73d44733.svg"
+    "url": "/IDerevyansky/deploystatic/media/vk.73d44733.svg"
   },
   {
     "revision": "641f3652b0a330e71c74c165a25c14bc",
-    "url": "/IDerevyansky/deploy/static/media/whatsapp-gray.641f3652.svg"
+    "url": "/IDerevyansky/deploystatic/media/whatsapp-gray.641f3652.svg"
   },
   {
     "revision": "df8d33ebfc12d3345916478725a5778f",
-    "url": "/IDerevyansky/deploy/static/media/whatsapp.df8d33eb.svg"
+    "url": "/IDerevyansky/deploystatic/media/whatsapp.df8d33eb.svg"
   }
 ]);
